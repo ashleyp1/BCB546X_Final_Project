935a30f515a4d6084dd3881db3e5950c217f5a5e
Ashley <ashleyp1@iastate.edu>
2018-11-28 15:43:50 -0600

    split excel file into two

--- Paulsen/final_project_AP.R
+++ Paulsen/final_project_AP.R
@@ -1,1 +1,9 @@
 ##final project code
+
+library(readxl)
+
+##had to go in excel and separate the sheets in the workbook into two diff workbooks
+microsatellite_genotype <- read_excel("Microsatellite genotype data.xlsx")
+sampling_info <- read_excel("sampling_information.xlsx")
+
+mtDNA_haplotypes <- read.table("Haplotypes of mtDNA-final.txt", sep = '\t', fill = T)
