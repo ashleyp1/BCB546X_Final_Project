bf980ab9cff47db52a90c49d23d1d12c15fc5ae8
Ashley <ashleyp1@iastate.edu>
2018-11-29 16:23:05 -0600

    sorted out the haplotypes into cleaner data frames

--- Paulsen/final_project_AP.R
+++ Paulsen/final_project_AP.R
@@ -7,3 +7,27 @@ microsatellite_genotype <- read_excel("Microsatellite genotype data.xlsx")
 sampling_info <- read_excel("sampling_information.xlsx")
 
 mtDNA_haplotypes <- read.table("Haplotypes of mtDNA-final.txt", sep = '\t', fill = T)
+
+
+##save haplotype data as characters for grep
+mtDNA_haplotypes[] <- lapply(mtDNA_haplotypes, as.character)
+
+id <- list()
+sequence <- list()
+location <- list()
+
+## go through and separate out the locations, sequences and ids in order
+for (row in 1:nrow(mtDNA_haplotypes)) {
+  n <- mtDNA_haplotypes[row, "V1"]
+  if(grepl('>', n)==TRUE) {
+    id <- c(id, n)
+  } else if(grepl('CAT', n)==TRUE){
+    sequence <- c(sequence, n)
+  } else {
+    location <- c(location, n)
+  }
+}
+
+##combine ids and sequences into pairs in data frame
+haplotypes <- do.call(rbind, Map(data.frame, ID = id, Sequence =sequence))
+
